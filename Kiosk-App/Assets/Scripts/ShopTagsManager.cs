3f9cc60bcb0b89f100aec931ff0f181d5096891b
Vince Delgado <v.delgado@student.saedubai.com>
2023-06-20 16:47:22 +0400

    Added reset color

--- Kiosk-App/Assets/Scripts/ShopTagsManager.cs
+++ Kiosk-App/Assets/Scripts/ShopTagsManager.cs
@@ -9,11 +9,14 @@ public class ShopTagsManager : MonoBehaviour
     [SerializeField] Button fashionTag;
     [SerializeField] Button restaurantTag;
 
+    [SerializeField] string[] shopTags;
+    [SerializeField] Renderer[] level1Shops;
+    [SerializeField] Renderer[] level2Shops;
     void Start()
     {
-        BtnListener(coffeeTag, "Coffee", Color.green);
-        BtnListener(fashionTag, "Fashion", Color.red);
-        BtnListener(restaurantTag, "Restaurant", Color.grey);
+        BtnListener(coffeeTag, shopTags[0], Color.green);
+        BtnListener(fashionTag, shopTags[1], Color.red);
+        BtnListener(restaurantTag, shopTags[2], Color.grey);
     }
 
     void BtnListener(Button btn, string tagName, Color shopHighLight)
@@ -23,10 +26,27 @@ public class ShopTagsManager : MonoBehaviour
 
     void HighlightShop(string tagName, Color shopColorHighlight)
     {
+        FindAllShopRenderers();
         GameObject[] shops = GameObject.FindGameObjectsWithTag(tagName);
         for (int i = 0; i < shops.Length; i++)
         {
             shops[i].GetComponent<Renderer>().material.color = shopColorHighlight;
+        }
+    }
+
+    void FindAllShopRenderers()
+    {
+        level1Shops = GameObject.Find("Level1").transform.Find("Shops").GetComponentsInChildren<Renderer>();
+        level2Shops = GameObject.Find("Level2").transform.Find("Shops").GetComponentsInChildren<Renderer>();
+        SetColor(level1Shops, Color.white);
+        SetColor(level2Shops, Color.white);
+    }
+
+    void SetColor(Renderer [] shops, Color buildingColor)
+    {
+        for (int i = 0; i < shops.Length; i++)
+        {
+            shops[i].material.color = buildingColor;
         }
     }
 }
